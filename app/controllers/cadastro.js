94f3d85de46529ba4b4619ce2c29a70cae0bf277
sanziowesley <sanziowesley@hotmail.com>
2021-03-07 21:56:37 -0300

    Tela de cadastro de usuários - recebendo e validando dados

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,22 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro');
+    res.render('cadastro', { validacao: {}, dadosForm: {} });
+}
+
+module.exports.cadastrar = function(application, req, res){
+    
+    var dadosForm = req.body;
+
+    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+    req.assert('casa', 'Casa não pode ser vazia').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
+        return;
+    }
+
+    res.send('podemos cadastrar');
 }
