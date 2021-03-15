e8b72bb3f2b2e36f31b7c8f5a0cf4740bf63c5ab
sanziowesley <sanziowesley@hotmail.com>
2021-03-15 20:28:26 -0300

    Ações com súditos - recebendo e validando dados

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -5,13 +5,18 @@ module.exports.jogo = function(application, req, res) {
         return;
     }
 
+    var comando_invalido = 'N';
+    if (req.query.comando_invalido == 'S') {
+        comando_invalido = 'S';
+    }
+
     var usuario = req.session.usuario;
     var casa = req.session.casa;
 
     var connection = application.config.dbConnection;
     var JogoDAO = new application.app.models.JogoDAO(connection);
 
-    JogoDAO.iniciaJogo(res, usuario, casa);
+    JogoDAO.iniciaJogo(res, usuario, casa, comando_invalido);
 }
 
 module.exports.sair = function(application, req, res) {
@@ -21,9 +26,41 @@ module.exports.sair = function(application, req, res) {
 }
 
 module.exports.suditos = function(application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.send('Usuário precisa fazer login');
+        return;
+    }
+
     res.render('aldeoes', { validacao: {} });
 }
 
 module.exports.pergaminhos = function(application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.send('Usuário precisa fazer login');
+        return;
+    }
+
     res.render('pergaminhos', { validacao: {} });
+}
+
+module.exports.ordenar_acao_sudito = function(application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.send('Usuário precisa fazer login');
+        return;
+    }
+
+    var dadosForm = req.body;
+
+    req.assert('acao', 'Ação deve ser informada').notEmpty();
+    req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.redirect('jogo?comando_invalido=S');
+        return;
+    }
+
+    console.log(dadosForm);
+    res.send('tudo ok');
 }
