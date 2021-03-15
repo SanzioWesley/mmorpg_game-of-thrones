e4f55cccc46e0f1f4d0e0b4b4aa26ffa932d5582
sanziowesley <sanziowesley@hotmail.com>
2021-03-15 19:03:55 -0300

    Exibindo pontos de habilidades, súditos e moedas

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,11 +1,18 @@
 module.exports.jogo = function(application, req, res) {
-    if (req.session.autorizado) {
-        res.render('jogo', { img_casa: req.session.casa });
-    } else {
-        res.send('Usuário precisa fazer login');
-    }
-}
 
+    if (req.session.autorizado !== true) {
+        res.send('Usuário precisa fazer login');
+        return;
+    }
+
+    var usuario = req.session.usuario;
+    var casa = req.session.casa;
+
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    JogoDAO.iniciaJogo(res, usuario, casa);
+}
 
 module.exports.sair = function(application, req, res) {
     req.session.destroy(function(err) {
