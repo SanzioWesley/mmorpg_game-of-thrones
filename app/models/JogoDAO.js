ecc9484f458e68c2cef5050a284467af440ea4e9
sanziowesley <sanziowesley@hotmail.com>
2021-03-16 22:41:24 -0300

    Atualizando moedas

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -58,6 +58,29 @@ JogoDAO.prototype.acao = function(acao) {
             acao.acao_termina_em = date.getTime() + tempo;
 
             collection.insert(acao);
+        });
+
+        mongoclient.collection("jogo", function(err, collection) {
+            var moedas = null;
+            switch (parseInt(acao.acao)) {
+                case 1:
+                    moedas = -2 * acao.quantidade;
+                    break;
+                case 2:
+                    moedas = -3 * acao.quantidade;
+                    break;
+                case 3:
+                    moedas = -1 * acao.quantidade;
+                    break;
+                case 4:
+                    moedas = -1 * acao.quantidade;
+                    break;
+            }
+            collection.update({ usuario: acao.usuario }, {
+                $inc: {
+                    moeda: moedas
+                }
+            });
 
             mongoclient.close();
         });
